feba0758a1de6bf6c03a89a149bf941a8da14aab
Justin Riley <20hz20khz@gmail.com>
2024-04-27 20:19:59 -0400

    Update Card.js

--- Card.js
+++ Card.js
@@ -1,18 +1,17 @@
 class Card {
-    constructor(value, suit, color) {
-        this.suit = suit;
-        this.value = value;
-        this.color = null;
-        if(suit==='♠' || suit==="♣"){
-            this.color == "black";
-        }
-        if(suit==='♥' || suit==="♦"){
-            this.color == "red";
-        }
+  constructor(value, suit, color) {
+    this.suit = suit;
+    this.value = value;
+    this.color = null;
+    if (suit === '♠' || suit === "♣") {
+      this.color == "black";
     }
+    if (suit === '♥' || suit === "♦") {
+      this.color == "red";
+    }
+  }
 
-    toString() {
-        return `${this.value} of ${this.suit}`;
-    }
+  toString() {
+    return `${this.value} ${this.suit}`;
+  }
 }
-
